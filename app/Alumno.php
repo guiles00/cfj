7a8b53dfbc0365ead7e5cc9d14b77cbcad46aec4
Guillermo A. Caserotto <gcaserotto@tsjbaires.gov.ar>
2019-08-07 14:11:44 -0300

    modificaciones del repo de gus

--- app/Alumno.php
+++ app/Alumno.php
@@ -1,6 +1,9 @@
 <?php namespace App;
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
 use Illuminate\Database\Eloquent\Model;
 
 class Alumno extends Model {
--- app/Tasks.php
+++ app/Tasks.php
@@ -1,5 +1,6 @@
 <?php namespace App;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Tasks extends Model {
